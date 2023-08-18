796e140e0159d99b422986c68286e6d9c90b02dd
SUBASHINI N <142592205+suba18rajan@users.noreply.github.com>
2023-08-18 18:18:01 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from login import Login_window
-from dashboard import RMS
-from tkinter import *
-import tkinter
-
-if __name__ == "__main__":
-    root = Tk()
-    obj = RMS(root)
-    root.mainloop()
